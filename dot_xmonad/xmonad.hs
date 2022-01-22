29920acee77a8cf8bd24f4a63704930551c82deb
Markus Burkert <gebolze@gmail.com>
2022-01-22 15:13:06 +0100

    Added wallpapers and set them in xmonad

--- dot_xmonad/xmonad.hs
+++ dot_xmonad/xmonad.hs
@@ -10,6 +10,7 @@ myTerminal = "alacritty"
 myStartupHook :: X ()
 myStartupHook = do
     spawnOnce "picom"
+    spawnOnce "feh --no-fehbg --bg-fill ~/.config/wallpapers/neon-samurai-cyberpunk.jpg ~/.config/wallpapers/digital_art_sunset.png"
 
 main = xmonad $ def
     { borderWidth = myBorderWidth
